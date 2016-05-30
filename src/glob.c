d8f2cc742c0f10385f88813e3aab3e1c6ad92afb
tonell_m <marceau.tonelli@epitech.eu>
2016-05-30 15:54:23 +0200

    glob.c + header indentation

---
+++ src/glob.c
@@ -0,0 +1,17 @@
+/*
+** glob.c for 42sh in /home/tonell-m/rendu/PSU/42sh
+**
+** Made by tonell_m
+** Login   <tonell-m@epitech.net>
+**
+** Started on  Mon May 30 15:47:22 2016 tonell_m
+** Last update Mon May 30 15:54:05 2016 tonell_m
+*/
+
+#include "sh.h"
+#include "my.h"
+
+int		glob(t_sh *sh, char *path)
+{
+
+}
