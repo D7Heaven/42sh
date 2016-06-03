1183d8b55e94024199b2cf1dcb645e71988dc2e3
tonell_m <marceau.tonelli@epitech.eu>
2016-06-03 15:20:49 +0200

    Normandie

--- src/tree.c
+++ src/tree.c
@@ -5,7 +5,7 @@
 ** Login   <jeanj@epitech.net>
 **
 ** Started on  Tue Apr 12 14:34:45 2016 Jean Jonathan
-** Last update Fri Jun  3 14:01:52 2016 tonell_m
+** Last update Fri Jun  3 15:20:20 2016 tonell_m
 */
 
 #include "my.h"
@@ -17,12 +17,12 @@ int		find_op(t_tree *tree, char *str, int end, char **ops)
   int		j;
   char		inhib;
 
-  j = 0;
-  while (j < MAX_OPS)
+  j = -1;
+  while (++j < MAX_OPS)
     {
-      i = end - 1;
+      i = end;
       inhib = 0;
-      while (i >= 0)
+      while (--i >= 0)
         {
 	  if (*(str + i) == '\"')
 	    inhib++;
@@ -35,9 +35,7 @@ int		find_op(t_tree *tree, char *str, int end, char **ops)
               tree->str = my_strdup(ops[j]);
               return (i + my_strlen(ops[j]));
             }
-          i--;
         }
-      j++;
     }
   return (-1);
 }
