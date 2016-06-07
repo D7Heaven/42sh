7dca1ac53b27ba5abc2c76bddbb900c1e0f31257
jean_j <jonathan.jean@epitech.eu>
2016-06-07 17:35:28 +0200

    Revert

--- src/message.c
+++ src/message.c
@@ -5,7 +5,7 @@
 ** Login   <jeanj@epitech.net>
 **
 ** Started on  Fri Jun  3 15:42:17 2016 Jean Jonathan
-** Last update Tue Jun  7 15:14:26 2016 Jean Jonathan
+** Last update Tue Jun  7 17:35:14 2016 Jean Jonathan
 */
 
 #include "sh.h"
@@ -16,7 +16,7 @@ void		handle_message(int status)
     {
       write(2, "Segmentation fault", 18);
       if (WCOREDUMP(status))
-        write(2, "(core dumped)", 14);
+        write(2, " (core dumped)", 14);
       write(2, "\n", 1);
       exit(139);
     }
