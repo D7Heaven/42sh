43455998742d39e9f93515139828069f7e816172
jean_j <jonathan.jean@epitech.eu>
2016-06-07 17:12:00 +0200

    Fix

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -5,10 +5,30 @@
 ** Login   <bedel_a@epitech.net>
 **
 ** Started on  Mon May 30 17:36:38 2016
-** Last update Tue Jun  7 18:18:55 2016 
+** Last update Tue Jun  7 17:11:50 2016 Jean Jonathan
 */
 
 #include "sh.h"
+
+int	is_it_ok(char *str)
+{
+  int	i;
+
+  if (!((str[0] >= 'a' && str[0] <= 'z') ||
+	(str[0] >= 'A' && str[0] <= 'Z')))
+    return (-2);
+  i = 0;
+  while (str[i] != '\0')
+    {
+      if (!(str[i] == '.' ||
+	    ((str[i] >= 'a' && str[i] <= 'z') ||
+	     (str[i] >= 'A' && str[i] <= 'Z')) ||
+	    (str[i] >= '0' && str[i] <= '9')))
+	return (-1);
+      i++;
+    }
+  return (1);
+}
 
 int	builtins_env(t_sh *sh)
 {
@@ -19,7 +39,17 @@ int	builtins_env(t_sh *sh)
 int     builtins_setenv(t_sh *sh)
 {
   if (sh->av[1] != NULL)
-    my_setenv(&sh->env, sh->av[1], sh->av[2]);
+    {
+      if (is_it_ok(sh->av[1]) == -1)
+        {
+          printf("setenv: Variable name ");
+          printf("must contain alphanumeric characters.\n");
+        }
+      else if (is_it_ok(sh->av[1]) == -2)
+        printf("setenv: Variable name must begin with a letter.\n");
+      else
+        my_setenv(&sh->env, sh->av[1], sh->av[2]);
+    }
   else
     print_list(sh->env);
   return (0);
@@ -41,24 +71,4 @@ int	builtins_unsetenv(t_sh *sh)
   else
     my_printf("unsetenv [key]\n");
   return (0);
-}
-
-int	is_it_ok(char *str)
-{
-  int	i;
-
-  if (!((str[0] >= 'a' && str[0] <= 'z') ||
-	(str[0] >= 'A' && str[0] <= 'Z')))
-    return (-1);
-  i = 0;
-  while (str[i] != '\0')
-    {
-      if (!(str[i] == '.' ||
-	    ((str[i] >= 'a' && str[i] <= 'z') ||
-	     (str[i] >= 'A' && str[i] <= 'Z')) ||
-	    (str[i] >= '0' && str[i] <= '9')))
-	return (-1);
-      i++;
-    }
-  return (1);
 }
