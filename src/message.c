df7738018301128d7b00d08ebffc87a3a303ccc0
jean_j <jonathan.jean@epitech.eu>
2016-06-07 13:41:25 +0200

    Message with coredumped

--- src/message.c
+++ src/message.c
@@ -5,7 +5,7 @@
 ** Login   <jeanj@epitech.net>
 **
 ** Started on  Fri Jun  3 15:42:17 2016 Jean Jonathan
-** Last update Fri Jun  3 18:17:17 2016 Jean Jonathan
+** Last update Tue Jun  7 13:37:53 2016 Jean Jonathan
 */
 
 #include "sh.h"
@@ -14,12 +14,18 @@ void		handle_message(int status)
 {
   if (WTERMSIG(status) == SIGSEGV)
     {
-      write(2, "Segmentation fault\n", 33);
+      write(2, "Segmentation fault", 18);
+      if (WCOREDUMP(status))
+        write(2, "(core dumped)", 13);
+      write(2, "\n", 1);
       exit(139);
     }
   else if (WTERMSIG(status) == SIGFPE)
     {
-      write(2, "Floating exception\n", 33);
+      write(2, "Floating exception", 18);
+      if (WCOREDUMP(status))
+        write(2, "(core dumped)", 13);
+      write(2, "\n", 1);
       exit(136);
     }
   else if (WTERMSIG(status) == SIGPIPE)
@@ -30,6 +36,4 @@ void		handle_message(int status)
     write(2, "Quit (core dumped)\n", 19);
   else if (WTERMSIG(status) == SIGABRT)
     write(2, "Abort (core dumped)\n", 20);
-  else if (WTERMSIG(status) == SIGBUS)
-    write(2, "Bus error (core dumped)\n", 24);
 }
